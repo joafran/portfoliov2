f2746146875f1e1948ecdcedb84b9030ef8ad27d
joafran <joafran0016@gmail.com>
2021-04-16 13:12:04 -0300

    updated icons

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,7 +1,8 @@
 import styles from './footer.module.css';
 import Fade from 'react-reveal/Fade'
-import { SiGithub, SiGmail, SiWhatsapp } from 'react-icons/si'
-import { FaLinkedinIn} from 'react-icons/fa'
+import { SiGithub, SiWhatsapp } from 'react-icons/si'
+import { RiLinkedinFill} from 'react-icons/ri'
+import { FiMail } from 'react-icons/fi'
 
 const Footer: React.FC = () => {
     return (
@@ -9,10 +10,14 @@ const Footer: React.FC = () => {
             <Fade>
                 <h1 className={styles.title}>Feel free to contact me!</h1>
                     <ul className={styles.list}>
-                        <li><a className={`${styles.link} ${styles.whatsapp}`} href="https://wa.me/543644709383"><SiWhatsapp /> </a></li>
-                        <li><a className={`${styles.link} ${styles.gmail}`} href="mailto:joafran0016@gmail.com"><SiGmail /></a></li>
-                        <li><a className={`${styles.link} ${styles.linkedin}`} href="https://linkedin.com/in/joaquin--franco"><FaLinkedinIn /></a></li>
-                        <li><a className={`${styles.link} ${styles.github}`} href="https://github.com/joafran"><SiGithub /></a></li>
+                        <li>
+                            <a className={`${styles.link} ${styles.whatsapp}`} href="https://wa.me/543644709383"><SiWhatsapp /> </a></li>
+                        <li>
+                            <a className={`${styles.link} ${styles.gmail}`} href="mailto:joafran0016@gmail.com"><FiMail /></a></li>
+                        <li>
+                            <a className={`${styles.link} ${styles.linkedin}`} href="https://linkedin.com/in/joaquin--franco"><RiLinkedinFill /></a></li>
+                        <li>
+                            <a className={`${styles.link} ${styles.github}`} href="https://github.com/joafran"><SiGithub /></a></li>
                     </ul>
                 <h3 className={styles.bottom}>{'<'} Made with a lot of coffee ☕ and patience by Joaquin Franco {'/>'}</h3>
             </Fade>
